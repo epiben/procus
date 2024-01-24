f8aca4108c61ad36a5bb228bcad0a68ae56135c3
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-01-24 11:03:25 +0100

    Using messaging service

--- send_sms.py
+++ send_sms.py
@@ -10,8 +10,9 @@ auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    body="Party in the CIA",
-    from_="+1 507 585 1781",
+    body="Party in the CIA, still",
+    # from_="+1 507 585 1781",
+    messaging_service_sid="MG06f833d7ac2df6991225efb31a9c8577",
     to="+4560196801"
 )
 
