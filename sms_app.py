88a08cd98ff781442ff21a28d8abc809277ce7f4
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-02-05 11:36:12 +0100

    Old Flask app

--- sms_app.py
+++ sms_app.py
@@ -1,11 +1,17 @@
 import random
-from flask import Flask
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from pprint import pprint
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=["GET", "POST"])
 def sms_reply():
+    pprint(request.values)
+
+    from_no = request.values.get("From", None)
+    print(from_no)
+
     resp = MessagingResponse()
 
     item = random.choice([
@@ -21,6 +27,8 @@ def sms_reply():
     """)
 
     # TODO: verify correct format (single-digit integer)
+    
+    resp = MessagingResponse()
     return str(resp)
 
 if __name__ == "__main__":
