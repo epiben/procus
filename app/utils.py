d22bba3922a3248e3890d849beff2d3d9612b19f
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-02-07 21:30:02 +0100

    Add docker-compose files

---
+++ app/utils.py
@@ -0,0 +1,64 @@
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+from psycopg2 import sql
+import logging
+import os
+import psycopg2
+
+load_dotenv(".env")
+
+def document_sms(
+        connection,
+        phone_number: str,
+        message_body: str,
+        direction: str
+        ) -> None:
+    timestamp = datetime.now(timezone.utc)
+    connection.cursor().execute(
+        """
+        INSERT INTO messages 
+            (sent_datetime, phone_number, message_body, direction)
+        VALUES (%s, %s, %s, %s);
+        """,
+        (timestamp, phone_number, message_body, direction)
+    )
+
+class DatabaseLogHandler(logging.Handler):
+    def __init__(self, connection_params, table_name="log"):
+        super(DatabaseLogHandler, self).__init__()
+        self.connection_params = connection_params
+        self.table_name = table_name
+        self.create_table()
+
+        self.insert_query = sql.SQL(
+            "INSERT INTO {} (level, message, created_at) VALUES (%s, %s, %s);"
+        ).format(sql.Identifier(self.table_name))
+
+    def create_table(self):
+        table_schema = sql.SQL("""
+            CREATE TABLE IF NOT EXISTS {} (
+                id SERIAL PRIMARY KEY,
+                level VARCHAR(10),
+                message TEXT,
+                created_at TIMESTAMP
+            )
+        """).format(sql.Identifier(self.table_name))
+
+        with psycopg2.connect(**self.connection_params) as conn:
+            conn.cursor().execute(table_schema)
+
+    def emit(self, record):
+        created_timestamp = datetime.fromtimestamp(record.created, timezone.utc)
+        with psycopg2.connect(**self.connection_params) as conn:
+            conn.cursor().execute(
+                self.insert_query, 
+                (record.levelname, record.msg, created_timestamp)
+            )
+
+DB_CONN_PARAMS = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "db",
+    "port": "5432"
+}
---
+++ postgres/init.sql
@@ -0,0 +1,114 @@
+-- SCHEMA
+CREATE TABLE public.recipients (
+    recipient_id SERIAL PRIMARY KEY,
+    phone_number text UNIQUE NOT NULL,
+    full_name text NOT NULL,
+    created_datetime timestamp with time zone DEFAULT now()
+);
+ALTER TABLE public.recipients OWNER TO postgres;
+
+
+CREATE TABLE public.instruments (
+    instrument_id SERIAL PRIMARY KEY,
+    instrument_name text NOT NULL,
+    is_active boolean DEFAULT true NOT NULL
+);
+ALTER TABLE public.instruments OWNER TO postgres;
+
+
+CREATE TABLE public.items (
+    item_id SERIAL PRIMARY KEY,
+    instrument_id integer REFERENCES public.instruments (instrument_id),
+    item_text text NOT NULL
+);
+ALTER TABLE public.items OWNER TO postgres;
+
+
+CREATE TABLE public.iterations (
+    iteration_id SERIAL PRIMARY KEY,
+    instrument_id integer REFERENCES public.instruments (instrument_id),
+    phone_number text NOT NULL REFERENCES recipients (phone_number),
+    message_body text NOT NULL,
+    is_open boolean DEFAULT false NOT NULL,
+    opens_datetime timestamp with time zone NOT NULL,
+    created_datetime timestamp with time zone DEFAULT now() NOT NULL
+);
+ALTER TABLE public.iterations OWNER TO postgres;
+CREATE INDEX idx__is_open ON public.iterations (is_open) WHERE is_open is not true; -- partial index, faster than full index
+
+
+CREATE TABLE public.responses (
+    response_id SERIAL PRIMARY KEY,
+    phone_number text REFERENCES recipients (phone_number),
+    item_id integer REFERENCES items (item_id),
+    item_text text,
+    created_datetime timestamp with time zone DEFAULT now(),
+    opens_datetime timestamp with time zone,
+    status_datetime timestamp with time zone DEFAULT now(),
+    response integer,
+    status text DEFAULT 'open'
+);
+ALTER TABLE public.responses OWNER TO postgres;
+
+
+CREATE TABLE public.log (
+    id SERIAL PRIMARY KEY,
+    level character varying(10),
+    message text,
+    created_at timestamp without time zone DEFAULT now()
+);
+ALTER TABLE public.log OWNER TO postgres;
+
+
+CREATE TABLE public.messages (
+    sent_datetime timestamp with time zone DEFAULT now() NOT NULL,
+    phone_number text REFERENCES recipients (phone_number),
+    message_body text,
+    direction text CHECK (direction IN ('outbound', 'inbound'))
+);
+ALTER TABLE public.messages OWNER TO postgres;
+
+COMMENT ON TABLE public.messages IS 
+'Holds all in- and outbound messages with timestamp, but without any tracking of which belong together. Table is meant for documentation and data scrutiny.';
+
+
+-- INITIAL DATA
+
+---- Instruments
+INSERT INTO public.instruments (instrument_name, is_active) 
+VALUES ('EQ-5D-5L', true);
+
+
+---- Items
+INSERT INTO public.items (instrument_id, item_text)
+VALUES 
+    (1, 'Hvor store problemer har du med at gå omkring?
+1: Ingen
+2: Lidt
+3: Moderate
+4: Store
+5: Jeg kan ikke gå omkring'),
+    (1, 'Hvor store problemer har du med at vaske eller klæde dig på? 
+1: Ingen
+2: Lidt
+3: Moderate
+4: Store
+5: Jeg kan ikke vaske mig eller klæde mig på'),
+    (1, 'Hvor store problemer har du med at udføre sædvanlige aktiviteter? 
+1: Ingen
+2: Lidt
+3: Moderate
+4: Store
+5: Jeg kan ikke udføre sædvanlige aktiviteter'),
+    (1, 'Hvor store smerter/meget ubehag har du? 
+1: Ingen
+2: Lidt
+3: Moderate
+4: Store
+5: Ekstreme'),
+    (1, 'I hvor høj grad er du ængstelig eller deprimeret? 
+1: Det er jeg ikke
+2: Lidt
+3: Moderate
+4: Store
+5: Ekstremt');
