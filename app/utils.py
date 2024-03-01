f5263b0dc2807cb7ba6eca1b9fedc3a247de0d98
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-03-02 00:14:41 +0100

    Linting

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,24 @@
+import logging
 from dataclasses import dataclass
 from datetime import datetime, timezone
+
+import psycopg2
 from dotenv import load_dotenv
 from fastapi import Response
 from psycopg2 import sql
-import logging
-import os
-import psycopg2
 
 load_dotenv(".env")
+
 
 # Set up custom response type
 class XmlResponse(Response):
     media_type = "application/xml;charset=utf-8"
 
+
 @dataclass
-class MessagingResponse():
+class MessagingResponse:
     # See https://www.cpsms.dk/files/CPSMS_2vejs_API.pdf
-    
+
     body: str = "<cancel></cancel>"
 
     def to_xml(self) -> XmlResponse:
@@ -24,31 +26,30 @@ class MessagingResponse():
             "<?xml version='1.0' encoding='utf-8'?>",
             "<reply>",
             self.body,
-            "</reply>"
+            "</reply>",
         ]
         return "\n".join(out)
-    
+
     def message(self, value) -> None:
         self.body = f"<message>{value}</message>"
 
     def no_response(self) -> None:
         self.body = "<cancel></cancel>"
 
+
 def document_sms(
-        connection,
-        phone_number: str,
-        message_body: str,
-        direction: str
-        ) -> None:
+    connection, phone_number: str, message_body: str, direction: str
+) -> None:
     timestamp = datetime.now(timezone.utc)
     connection.cursor().execute(
         """
-        INSERT INTO messages 
+        INSERT INTO messages
             (sent_datetime, phone_number, message_body, direction)
         VALUES (%s, %s, %s, %s);
         """,
-        (timestamp, phone_number, message_body, direction)
+        (timestamp, phone_number, message_body, direction),
     )
+
 
 class DatabaseLogHandler(logging.Handler):
     def __init__(self, connection_params, table_name="log"):
@@ -62,30 +63,36 @@ class DatabaseLogHandler(logging.Handler):
         ).format(sql.Identifier(self.table_name))
 
     def create_table(self):
-        table_schema = sql.SQL("""
+        table_schema = sql.SQL(
+            """
             CREATE TABLE IF NOT EXISTS {} (
                 id SERIAL PRIMARY KEY,
                 level TEXT,
                 message TEXT,
                 created_at TIMESTAMP
             )
-        """).format(sql.Identifier(self.table_name))
+        """
+        ).format(sql.Identifier(self.table_name))
 
         with psycopg2.connect(**self.connection_params) as conn:
             conn.cursor().execute(table_schema)
 
     def emit(self, record):
-        created_timestamp = datetime.fromtimestamp(record.created, timezone.utc)
+        created_timestamp = datetime.fromtimestamp(
+            record.created,
+            timezone.utc
+        )
         with psycopg2.connect(**self.connection_params) as conn:
             conn.cursor().execute(
-                self.insert_query, 
-                (record.levelname, record.msg, created_timestamp)
+                self.insert_query,
+                (record.levelname, record.msg, created_timestamp),
             )
+
 
 DB_CONN_PARAMS = {
     "dbname": "postgres",
     "user": "postgres",
     "password": "postgres",
     "host": "db",
-    "port": "5432"
+    "port": "5432",
 }
